80fc1c8167209ac2fa0feb6dfa51ba8c4c31f781
Dmitry <tkadim@gmail.com>
2026-09-26 11:48:33 +0300

    additional info section with phone, BD & avatar added ot Users in admin panel

--- BookStore/accounts/admin.py
+++ BookStore/accounts/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from accounts.models import CustomUser
 
-CustomUser._meta.app_label = 'auth'
+# CustomUser._meta.app_label = 'auth'
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("phone_number", "birth_date", "avatar")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("phone_number", "birth_date", "avatar")}),
+    )
